4c48e0e6745f56890872b81b1734d2354c28754f
AmanDeep9925 <amandeep58257@gmail.com>
2017-08-04 13:23:58 +0530

    added length code

---
+++ length.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+	Node *next;
+	int Data;
+	Node(int n)
+	{
+		Data = n;
+		next = NULL;
+	}
+};
+
+// this function will add a node to our head list
+
+Node * insert(Node *head, int val)
+{
+	Node *temp = new Node(val);
+
+	// if this node is thge first node
+	//i.e the head is still empty so
+	if (head == NULL)
+	{
+		return temp;
+	}
+	else
+		temp->next = head;
+	return temp;
+}
+
+// now let's print our list
+
+void printll(Node * head)
+{
+	// this fnc will simply do that it is
+	// printing our list data
+	Node * cur = head;
+	while (cur) // upto when cur exist
+	{
+		cout << cur->Data<<"->>";
+		cur = cur->next; // this next pointer will movw cur to next node
+	}
+}
+
+Node * createll(int N)
+{	// N here represent how much values do yo want to enter
+	Node* head = NULL;
+	for (int i = 0; i < N; ++i)
+	{	int x;
+		cin >> x;
+		head = insert(head, x);
+	}
+	return head;
+}
+// this code will return the length of our linked list
+int lengthLL(Node * head) {
+	int cnt = 0;
+	while (head) {
+		++cnt;
+		head = head->next;
+	}
+	return cnt;
+}
+
+int main()
+{
+	Node * head = NULL;
+	int N;
+	cin >> N;
+	head = createll(N);
+	printll(head);
+	int len= lengthLL(head);
+	cout<<"\n"<<len;
+}
