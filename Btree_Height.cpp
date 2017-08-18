c476f047f165de973c6fc16493cb0ec2542f0d9d
AmanDeep9925 <amandeep58257@gmail.com>
2017-08-18 22:14:59 +0530

    height of the binary tree

---
+++ Btree_Height.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* left;
+	Node* right;
+	Node(int D)
+	{
+		data = D;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node* creatree()
+{
+	int d;
+	cin >> d;
+	if (d == -1)
+	{
+		return NULL;
+	}
+	Node* root = new Node(d);
+	root->left = creatree();
+	root->right = creatree();
+	return root;
+}
+
+void preorder(Node* root)
+{
+	if (root == NULL)
+	{
+		return ;
+	}
+
+	cout << root->data << " " ;
+	preorder(root->left);
+	preorder(root->right);
+}
+
+int Btree_height(Node* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	return max(Btree_height(root->left), Btree_height(root->left)) + 1;
+}
+
+int main()
+{
+	Node* root = creatree();
+	preorder(root);
+	cout << endl;
+	int ans = Btree_height(root);
+	cout << ans;
+}
